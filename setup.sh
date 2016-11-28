0bbf6c44cc0494f2a67a1a31f79339d2fa5a9646
Duc Dao <huuduc.uneti@gmail.com>
2016-11-28 19:24:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ init() {
 	RED=`tput setaf 1`
 	GREEN=`tput setaf 2`
 	BLUE=`tput setaf 3`
-	if [[ $UID != 0 ]]; then
+	if [ $UID != 0 ]; then
 	    echo $RED "Please run this as root" $ENDC
 	    echo $BLUE "sudo $0 $*" $ENDC
 	    exit 0
@@ -16,10 +16,10 @@ run() {
 	THEMEPATH="/boot/grub/themes"
 	THEMENAME="UbuntuGrub"
 	FULLPATH=$THEMEPATH'/'$THEMENAME
-	if ![[ -d $THEMEPATH ]]; then
+	if ![ -d $THEMEPATH ]; then
 		sudo mkdir $THEMEPATH
 	fi
-	if [[ -d $FULLPATH ]]; then
+	if [ -d $FULLPATH ]; then
 		sudo rm -rf $FULLPATH
 	else
 		sudo mkdir $FULLPATH
@@ -29,7 +29,7 @@ run() {
 
 result() {
 	THEMEINFO="theme.txt"
-	if [[ -s $FULLPATH'/'$THEMEINFO ]]; then
+	if [ -s $FULLPATH'/'$THEMEINFO ]; then
 		echo $GREEN "All done!" $ENDC
 	else
 		echo $RED "Fail, run again!" $ENDC
