75b0eb261a44853388e45aee7f0e0a357cb5b37c
Duc Dao <ducdh1@smartosc.com>
2016-11-28 12:01:03 +0700

    Update background and add setup script

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+init() {
+	ENDC=`tput setaf 7`
+	RED=`tput setaf 1`
+	GREEN=`tput setaf 2`
+	BLUE=`tput setaf 3`
+	if [[ $UID != 0 ]]; then
+	    echo $RED "Please run this as root" $ENDC
+	    echo $BLUE "sudo $0 $*" $ENDC
+	    exit 0
+	fi	
+}
+
+run() {
+	THEMEPATH="/boot/grub/themes"
+	THEMENAME="UbuntuGrub"
+	FULLPATH=$THEMEPATH'/'$THEMENAME
+	if ![[ -d $THEMEPATH ]]; then
+		sudo mkdir $THEMEPATH
+	fi
+	if [[ -d $FULLPATH ]]; then
+		sudo rm -rf $FULLPATH
+	else
+		sudo mkdir $FULLPATH
+	fi	
+	sudo cp -r . $FULLPATH
+}
+
+result() {
+	THEMEINFO="theme.txt"
+	if [[ -s $FULLPATH'/'$THEMEINFO ]]; then
+		echo $GREEN "All done!" $ENDC
+	else
+		echo $RED "Fail, run again!" $ENDC
+	fi	
+}
+
+init
+run
+result
